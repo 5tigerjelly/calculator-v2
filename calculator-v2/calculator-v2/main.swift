b2752c2404821440b951f99ef9e91e2552953263
Chris Oh <5tigerjelly@gmail.com>
2016-04-06 16:31:47 -0700

    add array calculation

--- calculator-v2/calculator-v2/main.swift
+++ calculator-v2/calculator-v2/main.swift
@@ -8,34 +8,48 @@
 
 import Foundation
 
-//generic "math operation" function
+
 let addFunc : (Int, Int) -> Int = { $0 + $1 }
 let subFunc : (Int, Int) -> Int = { $0 - $1 }
 let mulFunc : (Int, Int) -> Int = { $0 * $1 }
 let divFunc : (Int, Int) -> Int = { $0 / $1 }
 let modFunc : (Int, Int) -> Int = { $0 % $1 }
 
-print("1 + 5 = \(addFunc(1,5))")
 print("5 - 4 = \(subFunc(5,4))")
 print("4 * 3 = \(mulFunc(4,3))")
 print("5 / 2 = \(divFunc(5,2))")
 print("6 % 4 = \(modFunc(6,4))")
 
-//Array fun
-func arrayAdd(numArray1 : [Int], numArray2 : [Int]) -> [Int]{
-   return []
-}
 
-
-
+//generic "math operation" function
 func mathOp(left : Int, right : Int, op : (Int, Int) -> Int) -> Int {
     return op(left, right)
 }
 
-//func mathOp(left : Int, right : Int, op : (Int, Int) -> Int) -> Int {
-//    return op(left, right)
-//}
-//let result = mathOp(1, right: 2) { $0 + $1 }
+print("1 + 8 = \(mathOp(1, right: 8) { $0 + $1 })")
+print("2 * 7 = \(mathOp(2, right: 7) { $0 * $1 })")
+print("6 / 2 = \(mathOp(6, right: 2) { $0 / $1 })")
+print("7 % 5 = \(mathOp(7, right: 5) { $0 % $1 })")
+print("5 - 4 = \(mathOp(5, right: 4) { $0 - $1 })")
+
+
+
+//Array fun
+func arrayAdd(numArray1 : [Int], numArray2 : [Int]) -> [Int]{
+    var result = [Int]()
+    for index in 0..<numArray1.count {
+        result += [(numArray1[index]+numArray2[index])]
+    }
+   return result
+}
+
+
+print("[1,2,3] + [2,3,4] = \(arrayAdd([1,2,3], numArray2: [2,3,4]))")
+
+
+
+
+let result = mathOp(1, right: 2) { $0 + $1 }
 //print("Result = \(result)")
 
 
